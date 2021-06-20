551c31a906775f36e901f4924f51a0371ed435b1
Laurent Berthelot <laurent.berthelot1969@gmail.com>
2021-06-20 17:35:11 +0200

    first test

--- src/test/java/fr/laurent/bowlingGame/entities/RollTest.java
+++ src/test/java/fr/laurent/bowlingGame/entities/RollTest.java
@@ -1,14 +1,24 @@
 package fr.laurent.bowlingGame.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Random;
 
 import org.junit.jupiter.api.Test;
 
 class RollTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testRandom() {
+		// Arrange
+		int pinsDown = 11;
+		// Act  
+		boolean ecart = 0 <= pinsDown && pinsDown<= 10;
+		// Assert
+		assertEquals(ecart, true);
 	}
 
 }

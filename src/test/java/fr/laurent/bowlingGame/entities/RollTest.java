d98a0f34a372198a0d535f8e32e076ba63af9cd6
Laurent Berthelot <laurent.berthelot1969@gmail.com>
2021-06-20 17:52:29 +0200

    test suite

--- src/test/java/fr/laurent/bowlingGame/entities/RollTest.java
+++ src/test/java/fr/laurent/bowlingGame/entities/RollTest.java
@@ -14,11 +14,24 @@ class RollTest {
 	@Test
 	void testRandom() {
 		// Arrange
-		int pinsDown = 11;
-		// Act  
-		boolean ecart = 0 <= pinsDown && pinsDown<= 10;
-		// Assert
-		assertEquals(ecart, true);
+		int i;
+		for(i = 0; i<100; i++) {
+			Random r = new Random();
+			int pinsDown = r.nextInt(10 + 1)+0;
+			//int pinsDown =5;
+			// Act  
+			boolean ecart = 0 <= pinsDown && pinsDown<= 10;
+			// Assert
+			if(!ecart) {
+				break;
+			};
+			
+		}
+		if(i == 100) {
+			System.out.println("test succed");
+		}else{
+			System.out.println("test failed");
+		};
 	}
 
 }
